b1ee9b0e3daeff9674e934a522399fbc6e55c484
Joanna Nguyen <jvn32@drexel.edu>
2024-08-11 19:15:49 -0400

    flask n such

---
+++ MOVIE RECOMMENDOR/Models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    preferences = db.Column(db.String(500))
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    movie_id = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
