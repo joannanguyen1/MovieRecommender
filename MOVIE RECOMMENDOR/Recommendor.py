9ed29666503456dd756c96a54ac61332715e4a07
Ben Nordeman <ben@macbook-pro-6.mynetworksettings.com>
2024-08-11 18:09:45 -0400

    s

--- MOVIE RECOMMENDOR/Recommendor.py
+++ MOVIE RECOMMENDOR/Recommendor.py
@@ -19,18 +19,19 @@ class Recommender:
         svd = TruncatedSVD(n_components=50, random_state=42) # instalizles the SVD model
         latent_matrix = svd.fit_transform(user_movie_matrix) 
         self.similarity_matrix = cosine_similarity(latent_matrix)
+        print(self.similarity_matrix)
 
-    def get_recommendations(self, user_id, top_n=10):
-        if self.similarity_matrix is None:
-            self.collaborative_filtering()
-
-        similar_users = self.similarity_matrix[user_id - 1]
-        similar_users_indices = similar_users.argsort()[::-1][:top_n]
-
-        recommended_movies = []
-        for idx in similar_users_indices:
-            similar_user_movies = self.ratings[self.ratings['userId'] == idx + 1]
-            recommended_movies.extend(similar_user_movies['movieId'].tolist())
-
-        return list(set(recommended_movies))
-    
+    #def get_recommendations(self, user_id, top_n=10):
+    #    if self.similarity_matrix is None:
+    #        self.collaborative_filtering()
+#
+    #    similar_users = self.similarity_matrix[user_id - 1]
+    #    similar_users_indices = similar_users.argsort()[::-1][:top_n]
+#
+    #    recommended_movies = []
+    #    for idx in similar_users_indices:
+    #        similar_user_movies = self.ratings[self.ratings['userId'] == idx + 1]
+    #        recommended_movies.extend(similar_user_movies['movieId'].tolist())
+#
+    #    return list(set(recommended_movies))
+    #
